4751553aeddc63aa37e411d3dd73a1cf125f5fe5
Laci <papplaszlonimrod@gmail.com>
2024-10-02 13:52:42 +0200

    while

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ b: bool = False
 
 #
 
-if a == True:
+"""  if a == True:
     print("a is True")
     pass
+elif b == False:
+    print("b is True")
+else:
+    print("neither a or b True")  """
 
-if b == False:
-    print("a is False")
-    pass
+# for loop
+# c : for(int i = 0; i < 5; i++)
+"""b = [0, 1, 2, 3, 4,]
+for i in range(5):
+    print(i)"""
+
+""" message: str = "Give me your answer: "
+while True:
+    answer: str = input(message)
+    if len(answer) >= 3:
+        print("Thank you!")
+        break
+    else:
+        print("Please give me at last 3 characters")"""
